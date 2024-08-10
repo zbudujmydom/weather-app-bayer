05cffa0f9f60fac11b5620aadc33e42a1f28b9c4
Tomek Sochacki <zbudujmydompl@gmail.com>
2024-08-10 10:14:03 +0200

    updated ids

--- js/form/data/cities.js
+++ js/form/data/cities.js
@@ -2,6 +2,6 @@ export const cities = [
   { id: 1, name: "Kraków", coordinates: { lat: 50.0614, lon: 19.9366 } },
   { id: 2, name: "Gdańsk", coordinates: { lat: 54.3523, lon: 18.6491 } },
   { id: 3, name: "Bydgoszcz", coordinates: { lat: 53.1235, lon: 18.0076 } },
-  { id: 3, name: "Zakopane", coordinates: { lat: 49.299, lon: 19.9489 } },
-  { id: 3, name: "Berlin", coordinates: { lat: 52.5244, lon: 13.4105 } },
+  { id: 4, name: "Zakopane", coordinates: { lat: 49.299, lon: 19.9489 } },
+  { id: 5, name: "Berlin", coordinates: { lat: 52.5244, lon: 13.4105 } },
 ];
